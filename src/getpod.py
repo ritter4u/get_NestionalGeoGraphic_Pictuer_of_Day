52bc5e6315605d803e29ad77904064be396fcce4
Keunreol Park <ritter4u@gmail.com>
2013-09-16 16:49:45 +0900

    2013-12-23 need to seperate as module

--- src/getpod.py
+++ src/getpod.py
@@ -6,13 +6,22 @@ import pprint
 """for debug"""
 def main():
 	page = urllib.request.urlopen('http://photography.nationalgeographic.com/photography/photo-of-the-day/')
-	html=page.read()
+	html=page.read().decode("ascii","ignore")
+	html=html.encode("utf8","xmlcharrefreplace")
+	html=str(html)
 	soup=BeautifulSoup(html)
-	print (soup.prettify())
-	#pp = pprint.PrettyPrinter(indent=4)
-	#pp.pprint(page)
-	
-	#sys.exit()
-	
+	#soup.prettify()
+	print (soup.select("#caption"))
+	print (soup.select("#caption"))
+	print (soup.select("#caption > .publication_time"))
+	print (soup.select("#caption > h2"))
+	print (soup.select("#caption > .credit"))
+	print (soup.select("#caption > p"))
+	list1=soup.select(".download_link > a")
+	for i in list1:
+		print (i.attrs['href'])
+	list2= soup.select(".prev > a")
+	for i2 in list2:
+		print (i2.attrs['href'])
 if __name__ == "__main__":
 	main()
