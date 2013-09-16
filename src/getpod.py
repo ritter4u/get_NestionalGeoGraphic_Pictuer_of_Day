dfa9fb03775272087764060e75601286b9b24129
Keunreol Park <ritter4u@gmail.com>
2013-09-16 16:28:27 +0900

    2013-09-16

--- src/getpod.py
+++ src/getpod.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import sys, urllib.request, os
-
+from bs4 import BeautifulSoup
 """for debug"""
 import pprint
 """for debug"""
-
 def main():
 	page = urllib.request.URLopener('http://photography.nationalgeographic.com/photography/photo-of-the-day/')
-	pp = pprint.PrettyPrinter(indent=4)
-	pp.pprint(page)
+	soup=BeautifulSoup(page)
+	print (soup.prettify())
+	#pp = pprint.PrettyPrinter(indent=4)
+	#pp.pprint(page)
 	
 	#sys.exit()
 	
