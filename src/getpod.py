950b5dc6f01b7a615e9d62bacc23397adb66e93f
Keunreol Park <ritter4u@gmail.com>
2014-01-02 15:12:05 +0900

    Revert "rerefactoring"
    This reverts commit 65f8f6520ccc49bb7d4c5f8e200ed777a3e76a05.

--- src/getpod.py
+++ src/getpod.py
@@ -1,46 +1,29 @@
 # -*- coding: utf-8 -*-
-import sys, urllib.request, os,sqlite3
+import sys, urllib.request, os
 from bs4 import BeautifulSoup
 
-url_prefix = "http://photography.nationalgeographic.com"
 
 """for debug"""
 import pprint
 """for debug"""
-def gethtml():
-	page = urllib.request.urlopen(url_prefix+'/photography/photo-of-the-day/')
-	html=page.read().decode("ascii","ignore")
-	return html
-def parseHtml(tmp):
-	tmp=tmp.replace('\n','')
-	tmp=tmp.replace('&laquo;','')
-	tmp=tmp.replace('&raquo;','')
-
-	data = {'html':tmp}
-	soup=BeautifulSoup(tmp)
-	#soup.prettify()
-	data['caption']=soup.select("#caption").pop()
-	data['publication_time']=soup.select("#caption > .publication_time").pop()
-	data['title']=soup.select("#caption > h2").pop()
-	data['credit']=soup.select("#caption > .credit").pop()
-	data['previous']=soup.select(".prev > a").pop().attrs['href']
-	data['download_link']=soup.select(".primary_photo img").pop().attrs['src']
-	data['image_description']=soup.select(".primary_photo img").pop().attrs['alt']
-	return data
-
 def main():
-	html=gethtml()
-	data=parseHtml(html)
-	print(data)
-
-
-#def initDatabase():
-
-#def store_to_database():
-#	print('aa')
-
-#def store_images():
-#	print('bb')
-
+	page = urllib.request.urlopen('http://photography.nationalgeographic.com/photography/photo-of-the-day/')
+	html=page.read().decode("ascii","ignore")
+	html=html.encode("utf8","xmlcharrefreplace")
+	html=str(html)
+	soup=BeautifulSoup(html)
+	#soup.prettify()
+	print (soup.select("#caption"))
+	print (soup.select("#caption"))
+	print (soup.select("#caption > .publication_time"))
+	print (soup.select("#caption > h2"))
+	print (soup.select("#caption > .credit"))
+	print (soup.select("#caption > p"))
+	list1=soup.select(".download_link > a")
+	for i in list1:
+		print (i.attrs['href'])
+	list2= soup.select(".prev > a")
+	for i2 in list2:
+		print (i2.attrs['href'])
 if __name__ == "__main__":
 	main()
