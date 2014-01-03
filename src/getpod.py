4414b544bd7441c8dde546b4ea57794b78e5220d
Keunreol Park <ritter4u@gmail.com>
2014-01-03 11:16:50 +0900

    TODO : exif 지원기능 추가 TODO : DB sqllite3 TODO : DB mysql TODO : DB mongodb TODO : app-engine TODO : django

--- src/getpod.py
+++ src/getpod.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
+#TODO : exif 지원기능 추가
+#TODO : DB sqllite3
+#TODO : DB mysql
+#TODO : DB mongodb
+#TODO : app-engine
+#TODO : django
 import sys, urllib.request, os,sqlite3
 from bs4 import BeautifulSoup
+
 
 url_prefix = "http://photography.nationalgeographic.com"
 
@@ -31,16 +38,28 @@ def parseHtml(tmp):
 def main():
 	html=gethtml()
 	data=parseHtml(html)
-	print(data)
+	image=store_images(data)
+	print(image)
 
+	#img = Image.open("test.jpg")
+	#exif = i._getexif()
+	# decode exif using TAGS
 
 #def initDatabase():
 
 #def store_to_database():
 #	print('aa')
 
-#def store_images():
-#	print('bb')
+def store_images(data):
+	url=data['download_link']
+	file_name = url.split('/')[-1]
+	u = urllib.request.urlopen(url)
+	meta = u.info()
+
+	f = open(file_name, 'wb')
+	f.write(u.read())
+	f.close()
+	return {'filename':file_name,'url':url}
 
 if __name__ == "__main__":
 	main()
