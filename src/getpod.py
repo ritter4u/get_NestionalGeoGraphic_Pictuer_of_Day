2caa79a6ccfb96392e0399f595015fc7888816b6
Keunreol Park <ritter4u@gmail.com>
2014-01-15 11:46:36 +0900

    add excrption for url link to get image

--- src/getpod.py
+++ src/getpod.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#TODO : class화 
 #TODO : exif 지원기능 추가
 #TODO : DB sqllite3
 #TODO : DB mysql
@@ -8,39 +9,76 @@
 import sys, urllib.request, os,sqlite3
 from bs4 import BeautifulSoup
 
+def connectSqlite3():
+	con = None
+	try:
+		con = sqlite3.connect('photo-of-the-day.db')
+		cur = con.cursor()    
+		cur.execute('SELECT SQLITE_VERSION()')  
+		data = cur.fetchone() 
+	except sqlite3.Error:
+		print ("Error %s:" % e.args[0])
+		sys.exit(1)
+	finally:
+		if con:
+			con.close()
+	return con
 
-url_prefix = "http://photography.nationalgeographic.com"
+class PhotoOfTheDay():
+	url_prefix = "http://photography.nationalgeographic.com"
+	data=None
+	info=None
 
-"""for debug"""
-import pprint
-"""for debug"""
-def gethtml():
-	page = urllib.request.urlopen(url_prefix+'/photography/photo-of-the-day/')
-	html=page.read().decode("ascii","ignore")
-	return html
-def parseHtml(tmp):
-	tmp=tmp.replace('\n','')
-	tmp=tmp.replace('&laquo;','')
-	tmp=tmp.replace('&raquo;','')
+	def getHtml(self,page_url=""):
+		if(page_url==""):
+			page_url=self.url_prefix+'/photography/photo-of-the-day/'
+		page = urllib.request.urlopen(page_url)
+		html=page.read().decode("ascii","ignore")
+		data = {'html':html}
+		self.data=data
+		
+	def parseHtml(self):
+		data = self.data
+		tmp=data['html']
+		tmp=tmp.replace('\n','')
+		tmp=tmp.replace('&laquo;','')
+		tmp=tmp.replace('&raquo;','')
+		soup=BeautifulSoup(tmp)
+		#soup.prettify()
+		data['caption']=soup.select("#caption").pop()
+		data['publication_time']=soup.select("#caption > .publication_time").pop()
+		data['title']=soup.select("#caption > h2").pop()
+		data['credit']=soup.select("#caption > .credit").pop()
+		data['previous']=soup.select(".prev > a").pop().attrs['href']
+		data['download_link']=soup.select(".primary_photo img").pop().attrs['src']
+		if(data['download_link'].find("http")==-1):
+			data['download_link']=data['download_link'].replace("//","http://")
+		data['image_description']=soup.select(".primary_photo img").pop().attrs['alt']
+		self.data=data
 
-	data = {'html':tmp}
-	soup=BeautifulSoup(tmp)
-	#soup.prettify()
-	data['caption']=soup.select("#caption").pop()
-	data['publication_time']=soup.select("#caption > .publication_time").pop()
-	data['title']=soup.select("#caption > h2").pop()
-	data['credit']=soup.select("#caption > .credit").pop()
-	data['previous']=soup.select(".prev > a").pop().attrs['href']
-	data['download_link']=soup.select(".primary_photo img").pop().attrs['src']
-	data['image_description']=soup.select(".primary_photo img").pop().attrs['alt']
-	return data
+	def store_images(self):
+		data=self.data
+		url=data['download_link']
+		file_name = url.split('/')[-1]
+		u = urllib.request.urlopen(url)
+		meta = u.info()
+
+		f = open(file_name, 'wb')
+		f.write(u.read())
+		f.close()
+		self.info={'filename':file_name,'url':url}
+#	def connectDB:
+#		return ""
 
 def main():
-	html=gethtml()
-	data=parseHtml(html)
-	image=store_images(data)
-	print(image)
+	pod = PhotoOfTheDay()
+	pod.getHtml()
+	pod.parseHtml()
+	pod.store_images()
+	print(pod.info)
+	#pod.connectSqlite3()
 
+	#
 	#img = Image.open("test.jpg")
 	#exif = i._getexif()
 	# decode exif using TAGS
@@ -50,16 +88,6 @@ def main():
 #def store_to_database():
 #	print('aa')
 
-def store_images(data):
-	url=data['download_link']
-	file_name = url.split('/')[-1]
-	u = urllib.request.urlopen(url)
-	meta = u.info()
-
-	f = open(file_name, 'wb')
-	f.write(u.read())
-	f.close()
-	return {'filename':file_name,'url':url}
-
 if __name__ == "__main__":
 	main()
+
